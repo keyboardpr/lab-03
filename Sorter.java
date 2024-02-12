481f534d10693950692103f5ffe2e63cd2c5ab88
waleed <wleeedo123@gmail.com>
2024-02-12 20:40:00 +0300

    <add insertionSort methode >

--- Sorter.java
+++ Sorter.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Sorter {
     public static void main(String[] args) {
+
         System.out.println("Hello");
+    }
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
     }
 }
